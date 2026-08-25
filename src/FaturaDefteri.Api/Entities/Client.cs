1467bfe77499e68bfd50319996029066e09a8a6f
Mert Aydın <mcanaydin60@trakya.edu.tr>
2026-08-25 22:53:24 +0300

    Müşteri varlığını ekle.

---
+++ src/FaturaDefteri.Api/Entities/Client.cs
@@ -0,0 +1,14 @@
+namespace FaturaDefteri.Api.Entities;
+
+public class Client
+{
+    public long Id { get; set; }
+    public string Name { get; set; } = "";
+    public string? TaxNumber { get; set; }
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+    public string? Address { get; set; }
+    public DateTime CreatedAtUtc { get; set; }
+
+    public List<Invoice> Invoices { get; set; } = [];
+}
