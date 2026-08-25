4df8f6c2161db57733f8c731747d3b587f342dd7
Mert Aydın <mcanaydin60@trakya.edu.tr>
2026-08-25 23:38:29 +0300

    Veritabanı context'i kullanıcı desteği ile güncellendi

--- src/FaturaDefteri.Api/Data/FaturaDbContext.cs
+++ src/FaturaDefteri.Api/Data/FaturaDbContext.cs
@@ -5,6 +5,7 @@ namespace FaturaDefteri.Api.Data;
 
 public class FaturaDbContext(DbContextOptions<FaturaDbContext> options) : DbContext(options)
 {
+    public DbSet<User> Users => Set<User>();
     public DbSet<IssuerProfile> IssuerProfiles => Set<IssuerProfile>();
     public DbSet<Client> Clients => Set<Client>();
     public DbSet<Invoice> Invoices => Set<Invoice>();
@@ -12,22 +13,33 @@ public class FaturaDbContext(DbContextOptions<FaturaDbContext> options) : DbCont
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<User>(e =>
+        {
+            e.Property(x => x.Email).HasMaxLength(200).IsRequired();
+            e.Property(x => x.DisplayName).HasMaxLength(200).IsRequired();
+            e.HasIndex(x => x.Email).IsUnique();
+        });
+
         modelBuilder.Entity<IssuerProfile>(e =>
         {
             e.Property(x => x.TradeName).HasMaxLength(160).IsRequired();
             e.Property(x => x.Currency).HasMaxLength(8);
+            e.HasIndex(x => x.UserId).IsUnique();
+            e.HasOne(x => x.User).WithMany().HasForeignKey(x => x.UserId).OnDelete(DeleteBehavior.Cascade);
         });
 
         modelBuilder.Entity<Client>(e =>
         {
             e.Property(x => x.Name).HasMaxLength(160).IsRequired();
-            e.HasIndex(x => x.Name);
+            e.HasIndex(x => new { x.UserId, x.Name });
+            e.HasOne(x => x.User).WithMany().HasForeignKey(x => x.UserId).OnDelete(DeleteBehavior.Cascade);
         });
 
         modelBuilder.Entity<Invoice>(e =>
         {
             e.Property(x => x.Number).HasMaxLength(32).IsRequired();
-            e.HasIndex(x => x.Number).IsUnique();
+            e.HasIndex(x => new { x.UserId, x.Number }).IsUnique();
+            e.HasOne(x => x.User).WithMany().HasForeignKey(x => x.UserId).OnDelete(DeleteBehavior.Cascade);
             e.HasMany(x => x.Lines).WithOne(x => x.Invoice).HasForeignKey(x => x.InvoiceId).OnDelete(DeleteBehavior.Cascade);
         });
 
